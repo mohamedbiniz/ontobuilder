8a73770bfa03eda80c360fa71eabab2600674416
tal.kellner <tal.kellner@gmail.com>
2011-07-01 20:35:56 +0000

    Place files into their target location.

---
+++ ontobuilder.gui/src/ac/technion/iem/ontobuilder/gui/tools/algorithms/line1/SimilarityFloodingAlgorithmGui.java
@@ -0,0 +1,60 @@
+package ac.technion.iem.ontobuilder.gui.tools.algorithms.line1;
+
+import javax.swing.JTable;
+
+import ac.technion.iem.ontobuilder.gui.application.ApplicationUtilities;
+import ac.technion.iem.ontobuilder.gui.application.PropertiesTableModel;
+import ac.technion.iem.ontobuilder.matching.algorithms.line1.misc.AbstractAlgorithm;
+import ac.technion.iem.ontobuilder.matching.algorithms.line1.misc.SimilarityFloodingAlgorithm;
+import ac.technion.iem.ontobuilder.matching.algorithms.line1.misc.SimilarityFloodingAlgorithmFixpointFormulasTypes;
+
+/**
+ * <p>
+ * Title: SimilarityFloodingAlgorithmGui
+ * </p>
+  * <p>Description: Implements the methods of the SimilarityFloodingAlgorithm used by the GUI</p>
+ */
+public class SimilarityFloodingAlgorithmGui
+{
+    SimilarityFloodingAlgorithm _similarityFloodingAlgorithm;
+    
+    public SimilarityFloodingAlgorithmGui(AbstractAlgorithm abstractAlgorithm)
+    {
+        _similarityFloodingAlgorithm = (SimilarityFloodingAlgorithm)abstractAlgorithm;
+    }
+    
+    // value of minimal residual vector length that will necessitate another algorithm iteration
+    protected double epsilon = 0.05;
+
+    // selected type of fixpoint formula
+    protected SimilarityFloodingAlgorithmFixpointFormulasTypes fixpointType = SimilarityFloodingAlgorithmFixpointFormulasTypes.FIX_BASIC; 
+    /**
+     * Constructs a default SimilarityFloodingAlgorithmGui
+     */
+    public SimilarityFloodingAlgorithmGui()
+    {
+    }
+
+    // creates the JTable for user to view and change values.
+    public JTable getProperties()
+    {
+        String columnNames[] =
+        {
+            ApplicationUtilities.getResourceString("properties.attribute"),
+            ApplicationUtilities.getResourceString("properties.value")
+        };
+        Object data[][] =
+        {
+            {
+                "Epsilon Value", new Double(epsilon)
+            },
+            {
+                "FixPoint Type", new Double(fixpointType.getValue())
+            }
+        };
+        JTable properties = new JTable(new PropertiesTableModel(columnNames, 2, data));
+        properties.setEditingRow(0);
+        properties.setEditingRow(1);
+        return properties;
+    }
+}
